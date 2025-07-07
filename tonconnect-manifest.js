e8327814cc49f1369f3b8f1f26f45ba8877be423
Stasoks <113279458+Stasoks@users.noreply.github.com>
2025-07-07 17:27:10 +0300

    Update tonconnect-manifest.js

--- tonconnect-manifest.js
+++ tonconnect-manifest.js
@@ -1,6 +1,6 @@
 {
         "name": "Roll NFT Game",
-        "url": "https://6a00-77-73-132-130.ngrok-free.app",
+        "url": "https://1246-77-73-132-130.ngrok-free.app",
         "iconUrl": "https://stasoks.github.io/tonconnect/icon.png",
         "termsUrl": "https://stasoks.github.io/tonconnect/terms.html",
         "privacyPolicyUrl": "https://stasoks.github.io/tonconnect/privacy.html"
